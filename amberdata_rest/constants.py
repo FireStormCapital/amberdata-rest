7598f410289f8592d4e159983701eb026256f9e3
Berk Ozdogan <berk.ozdogan@firestorm.capital>
2025-06-03 11:13:34 -0400

    - Add support for DeFi service - Create decorator for handling timestamps in DeFi service (non-raw methods) - Update common to handle multi level dicts data sets - Update dependencies and add pytest-xdist for parallel test running - Add new constants for DeFi - Update endpoints to latest end points - Rename spot test service file to the correct name

--- amberdata_rest/constants.py
+++ amberdata_rest/constants.py
@@ -38,17 +38,96 @@ AMBERDATA_SWAPS_REST_BATCH_FUNDING_RATES_ENDPOINT = "https://api.amberdata.com/m
 AMBERDATA_SWAPS_REST_BATCH_OHLCV_ENDPOINT = "https://api.amberdata.com/market/swaps/ohlcv/"
 AMBERDATA_SWAPS_REST_BATCH_OPEN_INTEREST_ENDPOINT = "https://api.amberdata.com/market/swaps/open-interest/"
 
-# Defi End Points
-AMBERDATA_DEFI_REST_INFORMATION_ENDPOINT = "https://web3api.io/api/v2/market/defi/"
-AMBERDATA_DEFI_REST_LENDING_ENDPOINT = "https://web3api.io/api/v2/market/defi/lending/exchanges/"
-AMBERDATA_DEFI_REST_LIQUIDITY_ENDPOINT = "https://web3api.io/api/v2/market/defi/liquidity/"
-AMBERDATA_DEFI_REST_LIQUIDITY_POOLS_ENDPOINT = "https://web3api.io/api/v2/market/defi/liquidity-positions/"
-AMBERDATA_DEFI_REST_OHLCV_ENDPOINT = "https://web3api.io/api/v2/market/defi/ohlcv"
-AMBERDATA_DEFI_REST_METRICS_ENDPOINT = "https://web3api.io/api/v2/market/defi/metrics/"
-AMBERDATA_DEFI_REST_TRADES_ENDPOINT = "https://web3api.io/api/v2/market/defi/trades/"
-AMBERDATA_DEFI_REST_TWAP_ENDPOINT = "https://web3api.io/api/v2/market/defi/twap/"
-AMBERDATA_DEFI_REST_VWAP_ENDPOINT = "https://web3api.io/api/v2/market/defi/vwap/"
-AMBERDATA_DEFI_REST_PRICES_ENDPOINT = "https://web3api.io/api/v2/market/defi/prices/"
+
+
+# 1 - DEX Trades Endpoints
+AMBERDATA_DEFI_REST_DEX_TRADES_HISTORICAL_ENDPOINT = "https://api.amberdata.com/defi/dex/trades"
+AMBERDATA_DEFI_REST_DEX_PROTOCOLS_INFORMATION_ENDPOINT = "https://api.amberdata.com/defi/dex/information"
+
+# 2 - OHLCV Endpoints
+AMBERDATA_DEFI_REST_OHLCV_INFORMATION_ENDPOINT = "https://api.amberdata.com/defi/dex/ohlcv/information"
+AMBERDATA_DEFI_REST_OHLCV_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/ohlcv/{pool}/latest"
+AMBERDATA_DEFI_REST_OHLCV_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/ohlcv/{pool}/historical"
+
+# 3 DEX LIQUIDITY ENDPOINTS
+AMBERDATA_DEFI_REST_LIQUIDITY_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/information"
+AMBERDATA_DEFI_REST_LIQUIDITY_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/{pool}/latest"
+AMBERDATA_DEFI_REST_LIQUIDITY_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/{pool}/historical"
+AMBERDATA_DEFI_REST_LIQUIDITY_SNAPSHOTS_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/{poolAddress}/snapshots"
+AMBERDATA_DEFI_REST_UNISWAP_V3_LIQUIDITY_DISTRIBUTION_ENDPOINT = "https://api.amberdata.com/defi/dex/uniswapv3/pools/{poolAddress}/liquidity-distribution"
+
+# 4 - LIQUIDITY PROVIDERS
+AMBERDATA_DEFI_REST_DEX_LIQUIDITY_POSITIONS_PAIRS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity-positions/pairs/{pair}/latest"
+AMBERDATA_DEFI_REST_DEX_LIQUIDITY_POSITIONS_PROVIDERS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity-positions/providers/{address}/latest"
+AMBERDATA_DEFI_REST_DEX_LIQUIDITY_PROVIDER_EVENTS_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/providers/{providerAddress}/events"
+
+# 5 - DEFI METRICS
+AMBERDATA_DEFI_REST_METRICS_EXCHANGES_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/latest"
+AMBERDATA_DEFI_REST_METRICS_EXCHANGES_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/historical"
+AMBERDATA_DEFI_REST_METRICS_ASSETS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/assets/{asset}/latest"
+AMBERDATA_DEFI_REST_METRICS_ASSETS_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/assets/{asset}/historical"
+AMBERDATA_DEFI_REST_METRICS_PAIRS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/pairs/{pair}/latest"
+AMBERDATA_DEFI_REST_METRICS_PAIRS_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/metrics/exchanges/{exchange}/pairs/{pair}/historical"
+
+# 6 - Price/TWAP/VWAP Endpoints
+AMBERDATA_DEFI_REST_ASSETS_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/prices/asset/information/"
+AMBERDATA_DEFI_REST_ASSET_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/prices/asset/{asset}/latest"
+AMBERDATA_DEFI_REST_ASSET_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/prices/asset/{asset}/historical"
+AMBERDATA_DEFI_REST_PAIRS_INFORMATION_ENDPOINT = "https://api.amberdata.com/defi/prices/pairs/information"
+AMBERDATA_DEFI_REST_PAIRS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/prices/pairs/bases/{base}/quotes/{quote}/latest"
+AMBERDATA_DEFI_REST_PAIRS_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/prices/pairs/bases/{base}/quotes/{quote}/historical"
+AMBERDATA_DEFI_REST_TWAP_ASSETS_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/twap/asset/information"
+AMBERDATA_DEFI_REST_TWAP_ASSET_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/twap/asset/{asset}/latest"
+AMBERDATA_DEFI_REST_TWAP_ASSET_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/twap/asset/{asset}/historical"
+AMBERDATA_DEFI_REST_TWAP_PAIRS_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/twap/pairs/information"
+AMBERDATA_DEFI_REST_TWAP_PAIRS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/twap/pairs/bases/{base}/quotes/{quote}/latest"
+AMBERDATA_DEFI_REST_TWAP_PAIRS_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/twap/pairs/bases/{base}/quotes/{quote}/historical"
+AMBERDATA_DEFI_REST_VWAP_ASSETS_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/asset/information"
+AMBERDATA_DEFI_REST_VWAP_ASSET_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/asset/{asset}/latest"
+AMBERDATA_DEFI_REST_VWAP_ASSET_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/asset/{asset}/historical"
+AMBERDATA_DEFI_REST_VWAP_PAIRS_INFORMATION_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/pairs/information"
+AMBERDATA_DEFI_REST_VWAP_PAIRS_LATEST_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/pairs/bases/{base}/quotes/{quote}/latest"
+AMBERDATA_DEFI_REST_VWAP_PAIRS_HISTORICAL_ENDPOINT = "https://api.amberdata.com/market/defi/vwap/pairs/bases/{base}/quotes/{quote}/historical"
+
+
+### 7 - DEX ALL TRANSACTIONS ###
+AMBERDATA_DEFI_REST_DEX_PROTOCOL_LENS_ENDPOINT = "https://api.amberdata.com/defi/dex/{protocolId}/protocol"
+AMBERDATA_DEFI_REST_DEX_POOL_LENS_ENDPOINT = "https://api.amberdata.com/defi/dex/{protocolId}/pools/{poolAddress}"
+AMBERDATA_DEFI_REST_DEX_WALLET_LENS_ENDPOINT = "https://api.amberdata.com/defi/dex/{protocolId}/wallets/{walletAddress}"
+
+# 8 - PORTFOLIO & RETURNS #
+AMBERDATA_DEFI_REST_PROVIDER_RETURN_SINCE_INCEPTION_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/providers/daily-return/{liquidityPoolAddress}"
+AMBERDATA_DEFI_REST_PROVIDER_HISTORICAL_RETURN_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/providers/return/{liquidityPoolAddress}"
+AMBERDATA_DEFI_REST_POOL_TOTAL_RETURN_ENDPOINT = "https://api.amberdata.com/market/defi/liquidity/pool/daily-return/{address}"
+AMBERDATA_DEFI_REST_WALLET_POSITIONS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/wallets/{address}/portfolio"
+AMBERDATA_DEFI_REST_PROFIT_LOSS_ENDPOINT = "https://api.amberdata.com/defi/lending/wallets/{walletAddress}/returns"
+AMBERDATA_DEFI_REST_IMPERMANENT_LOSS_ENDPOINT = "https://api.amberdata.com/defi/dex/wallets/{walletAddress}/returns"
+
+
+# 9 - Lending Endpoints
+AMBERDATA_DEFI_REST_LENDING_PROTOCOL_METRICS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/metrics/summary"
+AMBERDATA_DEFI_REST_LENDING_ASSET_METRICS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/assets/{assetId}/metrics/summary"
+
+### 10 - LENDING ALL TRANSACTIONS ###
+AMBERDATA_DEFI_REST_LENDING_PROTOCOL_LENS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/protocol"
+AMBERDATA_DEFI_REST_LENDING_ASSET_LENS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/assets/{asset}"
+AMBERDATA_DEFI_REST_LENDING_WALLET_LENS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/wallets/{walletAddress}"
+AMBERDATA_DEFI_REST_LENDING_GOVERNANCE_LENS_ENDPOINT = "https://api.amberdata.com/defi/lending/{protocolId}/governance"
+
+# 11 - LENDING STABLECOINS AGGREGATE INSIGHTS #
+AMBERDATA_DEFI_REST_LENDING_STABLECOINS_AGGREGATE_INSIGHTS_ENDPOINT = "https://api.amberdata.com/defi/stablecoins/{assetSymbol}/lending/metrics/summary"
+
+# 12 INFORMATION - LENDING PROTOCOLS #
+AMBERDATA_DEFI_REST_LENDING_PROTOCOLS_INFORMATION_ENDPOINT = "https://api.amberdata.com/defi/lending/protocols/information"
+
+# 13 - INFORMATION - ASSETS IN LENDING PROTOCOLS #
+AMBERDATA_DEFI_REST_LENDING_ASSETS_INFORMATION_ENDPOINT = "https://api.amberdata.com/defi/lending/assets/information"
+
+# 14 - INFORMATION - DEX PROTOCOLS #
+AMBERDATA_DEFI_REST_DEX_EXCHANGES_ENDPOINT = "https://api.amberdata.com/market/defi/dex/exchanges"
+
+# 15 - INFORMATION - PAIRS IN DEX PROTOCOLS #
+AMBERDATA_DEFI_REST_DEX_PAIRS_ENDPOINT = "https://api.amberdata.com/market/defi/dex/pairs"
 
 
 class MarketDataVenue(str, Enum):
@@ -91,14 +170,43 @@ class DexDataVenue(str, Enum):
     PANCAKESWAP = "Pancake LPs"
     CRODEFISWAP = "CroDefiSwap"
 
+class ProtocolId(str, Enum):
+    UNISWAP_V2 = "uniswapv2"
+    UNISWAP_V3 = "uniswapv3"
+    SUSHISWAP = "sushiswap"
+    CURVE_V1 = "curve"
+    AAVE_V2 = "aavev2"
+    AAVE_V3 = "aavev3"
+    COMPOUND_V2 = "compoundv2"
+    COMPOUND_V3 = "compoundv3"
+    makerdao = "makerdao"
+
+
+class ProtocolAction(str, Enum):
+    PAIR_CREATED = "PairCreated"
+    POOL_CREATED = "PoolCreated"
+    DEPOSIT = "Deposit"
+    WITHDRAW = "Withdraw"
+    LIQUIDATION_CALL = "LiquidationCall"
+    REPAY = "Repay"
+    BORROW = "Borrow"
+    FLASH_LOAN = "FlashLoan"
+
+class StableCoin(str, Enum):
+    USDC = "USDC"
+    USDT = "USDT"
+    DAI = "DAI"
+    BUSD = "BUSD"
+    TUSD = "TUSD"
+
 class LendingProtocol(str, Enum):
-    AAVE_V1 = "aave_v1"
-    AAVE_V2 = "aave_v2"
-    AAVE_V3 = "aave_v3"
-    COMPOUND_V1 = "compound_v1"
-    CREAM_V1 = "cream_v1"
-    CREAM_V2 = "cream_v2"
-    FORTUBE_V1 = "fortube_v1"
+    AAVE_V1 = "aave"
+    AAVE_V2 = "aavev2"
+    AAVE_V3 = "aavev3"
+    COMPOUND_V1 = "compound"
+    COMPOUND_V2 = "compoundv2"
+    COMPOUND_V3 = "compoundv3"
+    MAKERDAO = "makerdao"
 
 
 class TimeInterval(Enum):
@@ -141,3 +249,19 @@ class DailyTime(Enum):
     T1600_P0400 = "T16:00+04:00"
     T1600_P0800 = "T16:00+08:00"
     T1600_P0900 = "T16:00+09:00"
+
+class Blockchain(Enum):
+    ETHEREUM_MAINNET = 'ethereum-mainnet'
+    POLYGON_MAINNET = 'polygon-mainnet'
+    AVALANCHE_MAINNET = 'avalanche-mainnet'
+    ARBITRUM_ONE_MAINNET = 'arb-one-mainnet'
+    BITCOIN_MAINNET = 'bitcoin-mainnet'
+    BITCOIN_CASH_MAINNET = 'bitcoin-abc-mainnet'
+    BINANCE_SMART_CHAIN_MAINNET = 'bnb-mainnet'
+    LITECOIN_MAINNET = 'litecoin-mainnet'
+    SOLANA_MAINNET = 'solana-mainnet'
+
+class DEXSortBy(Enum):
+    NUM_PAIRS = 'numPairs'
+    VOLUME_USD = 'volumeUSD'
+    # Add other sorting options as needed
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='amberdata_rest',
     packages=find_packages(include=['amberdata_rest', 'amberdata_rest.*']),
-    version='0.2.1',
+    version='0.3.0',
     description='Amberdata.io REST API SDK',
     author='technology@firestorm.capital',
     install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==8.2.2'],
+    setup_requires=['pytest-runner', 'pytest-xdist'],
+    tests_require=['pytest==8.2.2', 'pytest-xdist==3.3.0'],
     test_suite='tests',
 )
